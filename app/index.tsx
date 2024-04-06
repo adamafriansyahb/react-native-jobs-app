3a0fa307f71b36c4130980e300b2a0213f4bf728
adamafriansyahb <adamafriansyahb@gmail.com>
2024-04-06 12:22:24 +0700

    install prettier, add assets, predefined styles and component skeletons

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+
+const Home = () => {
+  return (
+    <View style={{ padding: 10 }}>
+      <Text style={{ fontSize: 24, fontWeight: 'bold', marginTop: 10 }}>
+        Home ANjay
+      </Text>
+    </View>
+  );
+};
+
+export default Home;
